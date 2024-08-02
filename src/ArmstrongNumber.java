c9d7b9e5d3c85887a8f2512f5e06a5266174935e
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-02 13:48:19 +0530

    Updated Armstrong.java

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class ArmstrongNumber {
+
+    static String isArmstrong(int num){
+
+        int number = num;
+        int rem,sum = 0;
+
+        while(num!=0){
+            rem = num%10;
+            sum = sum + rem * rem * rem;
+            num /= 10;
+
+        }
+        if(number == sum){
+            return number + " is an Armstrong Number.";
+        }
+        else {
+            return number + " is not an Armstrong Number.";
+        }
+
+    }
 
     public static void main(String[] args){
 
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(isArmstrong(number));
+
+
     }
 
 }

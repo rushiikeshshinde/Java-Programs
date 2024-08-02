e75405cb1fcb0353eccabc6410b1ac73fb61ba43
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-02 14:49:39 +0530

    Added ReversedNumber.java

---
+++ src/Number_Programs/ReverseNumber.java
@@ -0,0 +1,26 @@
+package Number_Programs;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    static String reverseNumber(int num){
+        int reversed_number = 0;
+
+        while (num!=0){
+            int rem = num % 10;
+            reversed_number = reversed_number * 10 + rem;
+            num /= 10;
+        }
+        return "Reversed Number: "+reversed_number;
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.print(reverseNumber(number));
+
+    }
+
+}

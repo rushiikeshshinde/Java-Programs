e820a31d3df263c3fa93ad639df183ff6e0d6d3a
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-03 11:08:12 +0530

    Added Functions

--- src/Basic_Programs/FibonacciSeries.java
+++ src/Basic_Programs/FibonacciSeries.java
@@ -1,18 +1,26 @@
 package Basic_Programs;
+import java.util.Scanner;
 
 public class FibonacciSeries {
 
-    public static void main(String[] args){
+    static void fibonacciSeries(int count){
 
-        int n1 = 0,n2 = 1,n3,i,count = 10;
+        int n1 = 0;
+        int n2 = 1,i,n3;
         System.out.print(n1+","+n2);
-
         for(i=2;i<count;++i){
             n3 = n1 + n2;
             System.out.print(","+n3);
             n1 = n2;
             n2 = n3;
-        }
+    }
+
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        fibonacciSeries(count);
 
     }
 

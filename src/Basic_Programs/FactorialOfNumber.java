731f6c0da91e4ccf1b4a99725e2ca71475dd3080
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-03 12:08:19 +0530

    Added FactorialOfNumber.java

---
+++ src/Basic_Programs/FactorialOfNumber.java
@@ -0,0 +1,25 @@
+package Basic_Programs;
+import java.util.Scanner;
+
+public class FactorialOfNumber {
+
+    static int factorialOfNumber(int num){
+
+        int factorial = 1;
+        for(int i = num; i>=1; i--){
+            factorial *= i;
+        }
+        return factorial;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        System.out.println(factorialOfNumber(number));
+
+    }
+
+}

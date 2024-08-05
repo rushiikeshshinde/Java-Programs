51d067c731e96a9499781bb898e05e0a4cba70f3
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-05 15:02:30 +0530

    Added FascinatingNumber.java

---
+++ src/Number_Programs/FascinatingNumber.java
@@ -0,0 +1,48 @@
+package Number_Programs;
+
+import java.util.Scanner;
+
+public class FascinatingNumber {
+
+    static void isFascinatingNumber(int num){
+
+        int n2,n3;
+        boolean found = true;
+
+        n2 = num * 2;
+        n3 = num * 3;
+
+        String concatStr = num + ""+n2+n3;
+
+        for (char c ='1'; c<='9';c++){
+            int count = 0;
+            for(int i = 0;i<concatStr.length();i++){
+                char ch = concatStr.charAt(i);
+
+                if(ch==c){
+                    count++;
+                }
+            }
+            if(count > 1 || count == 0){
+                found = false;
+                break;
+            }
+        }
+        if(found){
+            System.out.println(num+ " is a Fascinating Number.");
+        }
+        else {
+            System.out.println(num+ " is not a Fascinating Number.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        isFascinatingNumber(number);
+
+    }
+
+}

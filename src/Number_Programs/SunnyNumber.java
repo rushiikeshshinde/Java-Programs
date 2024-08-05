b169e7b5544e4dadd3445f2d082f2cb1eeac83c5
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-05 14:13:02 +0530

    Added SunnyNumber.java

---
+++ src/Number_Programs/SunnyNumber.java
@@ -0,0 +1,33 @@
+package Number_Programs;
+import java.util.Scanner;
+
+public class SunnyNumber {
+
+    static boolean isPerfectNumber(double num){
+
+        double square_root = Math.sqrt(num);
+
+        return (square_root - Math.floor(square_root)) == 0;
+
+    }
+
+    static void isSunnyNumber(int num){
+
+        if(isPerfectNumber(num+1)){
+            System.out.println("The given number is Sunny Number.");
+        }
+        else {
+            System.out.println("The given number is not Sunny Number.");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        isSunnyNumber(number);
+
+    }
+
+}

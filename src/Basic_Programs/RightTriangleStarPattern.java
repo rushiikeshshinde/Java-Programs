cd735ed01264323d3e55507d5987518ceeb95727
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-05 11:06:54 +0530

    Updated RighTriangleStarPattern.java

--- src/Basic_Programs/RightTriangleStarPattern.java
+++ src/Basic_Programs/RightTriangleStarPattern.java
@@ -7,7 +7,7 @@ public class RightTriangleStarPattern {
 
         for(i=1;i<=n;i++){
             for(j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
